500a34844af53bedad2f19342ea687912c1c7587
Andrew <andrewmeier.dev@gmail.com>
2026-05-04 22:09:04 -0300

    codex changes

--- api/autos.js
+++ api/autos.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   try {
     const response = await fetch("https://oracleapex.com/ords/tbdandres/alquiler/autos/", {
       method: "GET",
@@ -12,8 +20,8 @@ export default async function handler(req, res) {
 
     const text = await response.text();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(text);
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.status(response.status).send(text);
 
   } catch (error) {
     res.status(500).json({ error: error.message });
