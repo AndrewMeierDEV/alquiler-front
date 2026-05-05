3b5b16234c94ebeecc08f8ddddd714e56beff507
Andrew <andrewmeier.dev@gmail.com>
2026-05-04 21:52:09 -0300

    api js2

--- api/autos.js
+++ api/autos.js
@@ -1,12 +1,21 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://oracleapex.com/ords/tbdandres/alquiler/autos/");
-    const data = await response.json();
+    const response = await fetch("https://oracleapex.com/ords/tbdandres/alquiler/autos/", {
+      method: "GET",
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json",
+        "Accept-Language": "es-ES,es;q=0.9",
+        "Connection": "keep-alive"
+      }
+    });
+
+    const text = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(data);
+    res.status(200).send(text);
 
   } catch (error) {
-    res.status(500).json({ error: "Error al obtener datos" });
+    res.status(500).json({ error: error.message });
   }
 }
